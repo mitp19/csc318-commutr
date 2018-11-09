89adf92e80dc1347cb3e439b636ef618b5f8a54d
Mit Patel <mitu.patel@mail.utoronto.ca>
2018-11-09 17:22:59 -0500

    Pushing where are you going card for commute tab

--- src/TabB.js
+++ src/TabB.js
@@ -1,9 +1,13 @@
 import React, { Component } from 'react'
 import {
   View,
-  Text,
   StyleSheet,
 } from 'react-native'
+
+import {
+  Container, Header, Content, Card, CardItem,
+  Text, Body, Button, Icon, Title, Left, Right, Item, Input
+} from "native-base";
 
 class TabB extends Component {
 
@@ -11,12 +15,61 @@ class TabB extends Component {
     title: 'Commute',
   })
 
-  render () {
+  render() {
     return (
-      <View style={styles.container}>
-        <Text style={styles.text}> Commute </Text>
-      </View>
-      )
+      <Container styles={styles.container}>
+        <Header>
+          <Left />
+          <Body>
+            <Title>Commute</Title>
+          </Body>
+          <Right />
+        </Header>
+        <Content padder>
+          <Card>
+            <CardItem header bordered>
+              <Text> Where are going?</Text>
+            </CardItem>
+            <CardItem bordered>
+              <Body>
+                <View style={{ flexDirection: "row", flex: 1, justifyContent: 'space-between' }}>
+                  <Button iconLeft>
+                    <Icon name='home' />
+                    <Text>Home</Text>
+                  </Button>
+                  <Button iconLeft primary style={{ left: 10 }}>
+                    <Icon name='briefcase' />
+                    <Text>Work</Text>
+                  </Button>
+                </View>
+              </Body>
+            </CardItem>
+            <CardItem bordered>
+              <Body style={{paddingBottom:10}}>
+                <Text>Recent Destinations</Text>
+                <Button block style={{ top: 5 }}>
+                  <Text>150 College St.</Text>
+                </Button>
+                <Button block style={{ top: 10 }}>
+                  <Text>39 Sunflower Rd.</Text>
+                </Button>
+              </Body>
+            </CardItem>
+            <CardItem>
+              <Body>
+                <Item>
+                  <Input placeholder='Enter New Destination' />
+                  <Icon active name='arrow-round-forward' />
+                </Item>
+              </Body>
+            </CardItem>
+
+            {/* </Body>
+            </CardItem> */}
+          </Card>
+        </Content>
+      </Container>
+    );
   }
 }
 
@@ -25,10 +78,10 @@ export default TabB
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
+    // alignItems: 'center',
+    // justifyContent: 'center',
     backgroundColor: '#8e44ad',
-    padding: 20,
+    // padding: 20,
   },
   text: {
     color: 'white',
