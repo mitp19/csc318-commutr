e2d6f6c2444f1c130886afe40699003352a041dd
Susan Wu <susanjy.wu@mail.utoronto.ca>
2018-11-15 03:24:37 -0500

    Changed Books/Articles, added Music, Podcast and Downloads screens

--- src/Media/Downloads.js
+++ src/Media/Downloads.js
@@ -7,14 +7,14 @@ import {
 } from 'react-native';
 
 import {
-  Container, Header, Icon, Picker, Left, Right, Button, Body, Title, Card, CardItem, Content
+  Container, Header, Icon, Picker, Left, Right, Button, Body, Title, Card, CardItem,Content
 } from 'native-base';
 
 import FontAwesome, { Icons } from 'react-native-fontawesome';
 
 import { SearchBar } from 'react-native-elements'
 
-class BooksArticles extends Component {
+class Downloads extends Component {
   
   // const mediaIcon = parseIcon('')
   static navigationOptions = ({ navigation }) => ({
@@ -29,7 +29,6 @@ class BooksArticles extends Component {
       selected: "key0"
     };
   }
-
   onValueChange(value: string) {
     this.setState({
       selected: value
@@ -52,7 +51,7 @@ class BooksArticles extends Component {
             </Button>
           </Left>
           <Body>
-          <Title>Books/Articles</Title>
+          <Title>Downloads</Title>
           </Body>
           <Right />
           <SearchBar
@@ -71,7 +70,7 @@ class BooksArticles extends Component {
                     </Button>
                   </Left>
                   <Body style={{ flex: 3 }}>
-                    <Title style={{ color: "#fff" }}>Choose a genre</Title>
+                    <Title style={{ color: "#fff" }}>Choose a category</Title>
                   </Body>
                   <Right />
                 </Header>}
@@ -80,44 +79,33 @@ class BooksArticles extends Component {
               selectedValue={this.state.selected}
               onValueChange={this.onValueChange.bind(this)}
             >
-              <Picker.Item label="Action/Adventure" value="key0"/>
-              <Picker.Item label="Classics" value="key1"/>
-              <Picker.Item label="Contemporary" value="key2" />
-              <Picker.Item label="Dystopia" value="key3" />
-              <Picker.Item label="Fiction" value="key4" />
-              <Picker.Item label="Historical Fiction" value="key5" />
-              <Picker.Item label="Horror" value="key6" />
-              <Picker.Item label="Magical Realism" value="key7" />
-              <Picker.Item label="Mystery/Thriller" value="key8" />
-              <Picker.Item label="Nonfiction" value="key9" />
-              <Picker.Item label="Poetry" value="key10" />
-              <Picker.Item label="Romance" value="key11" />
-              <Picker.Item label="Sci-fi/Fantasy" value="key12" />
-              <Picker.Item label="Young Adult" value="key13" />
-              <Picker.Item label="Other" value="key14" />
+              <Picker.Item label="View All" value="key0"/>
+              <Picker.Item label="Books/Articles" value="key1"/>
+              <Picker.Item label="Music" value="key2" />
+              <Picker.Item label="Podcasts" value="key3" />
             </Picker>
             <Content padder>
         <Card>
           <CardItem bordered style={{width: 280}}>
             <View>
-              <Text style={{fontSize: 14, fontWeight: 'bold'}}>The Astonishing Color of After</Text>
-              <Text style={{fontSize: 11}}>by Emily X.R. Pan</Text>
+              <Text style={{fontSize: 14, fontWeight: 'bold'}}>Party For One</Text>
+              <Text style={{fontSize: 11}}>by Carly Rae Jepsen</Text>
             </View>
             <View>
               <Button transparent 
-              style={{marginLeft: 80}} 
+              style={{marginLeft: 175}} 
                   >
                 <Icon 
-                  name='download' 
-                  type='Feather' 
-                  style={{fontSize: 20, color: 'black'}}/>
+                  name='ios-trash' 
+                  type='Ionicons' 
+                  style={{fontSize: 26, color: 'black'}}/>
               </Button>
               <Button 
               transparent 
-              style={{marginLeft: 80}}
+              style={{marginLeft: 172}}
                   >
                 <Icon 
-                  name='open-in-new' 
+                  name='play-circle' 
                   type='MaterialCommunityIcons' 
                   style={{fontSize: 20, color: 'black'}}/>
               </Button>
@@ -127,24 +115,24 @@ class BooksArticles extends Component {
         <Card>
           <CardItem bordered style={{width: 280}}>
             <View>
-              <Text style={{fontSize: 14, fontWeight: 'bold'}}>Beartown</Text>
-              <Text style={{fontSize: 11}}>by Fredrik Backman</Text>
+              <Text style={{fontSize: 14, fontWeight: 'bold'}}>Rose-Colored Boy</Text>
+              <Text style={{fontSize: 11}}>by Paramore</Text>
             </View>
             <View>
               <Button transparent 
-              style={{marginLeft: 184}} 
+              style={{marginLeft: 158}} 
                   >
-                <Icon
-                  name='download' 
-                  type='Feather' 
-                  style={{fontSize: 20, color: 'black'}}/>
+                <Icon 
+                  name='ios-trash' 
+                  type='Ionicons' 
+                  style={{fontSize: 26, color: 'black'}}/>
               </Button>
               <Button 
               transparent 
-              style={{marginLeft: 184}}
+              style={{marginLeft: 155}}
                   >
                 <Icon 
-                  name='open-in-new' 
+                  name='play-circle' 
                   type='MaterialCommunityIcons' 
                   style={{fontSize: 20, color: 'black'}}/>
               </Button>
@@ -154,24 +142,24 @@ class BooksArticles extends Component {
         <Card>
           <CardItem bordered style={{width: 280}}>
             <View>
-              <Text style={{fontSize: 14, fontWeight: 'bold'}}>A Gentleman in Moscow</Text>
-              <Text style={{fontSize: 11}}>by Amor Towles</Text>
+              <Text style={{fontSize: 14, fontWeight: 'bold'}}>So Sad, So Sad</Text>
+              <Text style={{fontSize: 11}}>by Varsity</Text>
             </View>
             <View>
               <Button transparent 
-              style={{marginLeft: 122}} 
+              style={{marginLeft: 179}} 
                   >
-                <Icon
-                  name='download' 
-                  type='Feather' 
-                  style={{fontSize: 20, color: 'black'}}/>
+                <Icon 
+                  name='ios-trash' 
+                  type='Ionicons' 
+                  style={{fontSize: 26, color: 'black'}}/>
               </Button>
               <Button 
               transparent 
-              style={{marginLeft: 122}}
+              style={{marginLeft: 176}}
                   >
                 <Icon 
-                  name='open-in-new' 
+                  name='play-circle' 
                   type='MaterialCommunityIcons' 
                   style={{fontSize: 20, color: 'black'}}/>
               </Button>
@@ -181,24 +169,24 @@ class BooksArticles extends Component {
         <Card>
           <CardItem bordered style={{width: 280}}>
             <View>
-              <Text style={{fontSize: 14, fontWeight: 'bold'}}>The Heart's Invisible Furies</Text>
-              <Text style={{fontSize: 11}}>by John Boyne</Text>
+              <Text style={{fontSize: 14, fontWeight: 'bold'}}>thank u, next</Text>
+              <Text style={{fontSize: 11}}>by Ariana Grande</Text>
             </View>
             <View>
               <Button transparent 
-              style={{marginLeft: 99}} 
+              style={{marginLeft: 191}} 
                   >
-                <Icon
-                  name='download' 
-                  type='Feather' 
-                  style={{fontSize: 20, color: 'black'}}/>
+                <Icon 
+                  name='ios-trash' 
+                  type='Ionicons' 
+                  style={{fontSize: 26, color: 'black'}}/>
               </Button>
               <Button 
               transparent 
-              style={{marginLeft: 99}}
+              style={{marginLeft: 188}}
                   >
                 <Icon 
-                  name='open-in-new' 
+                  name='play-circle' 
                   type='MaterialCommunityIcons' 
                   style={{fontSize: 20, color: 'black'}}/>
               </Button>
@@ -208,24 +196,24 @@ class BooksArticles extends Component {
         <Card>
           <CardItem bordered style={{width: 280}}>
             <View>
-              <Text style={{fontSize: 14, fontWeight: 'bold'}}>Little Fires Everywhere</Text>
-              <Text style={{fontSize: 11}}>by Celeste Ng</Text>
+              <Text style={{fontSize: 14, fontWeight: 'bold'}}>We Belong Together</Text>
+              <Text style={{fontSize: 11}}>by Mariah Carey</Text>
             </View>
             <View>
               <Button transparent 
-              style={{marginLeft: 127}} 
+              style={{marginLeft: 145}} 
                   >
-                <Icon
-                  name='download' 
-                  type='Feather' 
-                  style={{fontSize: 20, color: 'black'}}/>
+                <Icon 
+                  name='ios-trash' 
+                  type='Ionicons' 
+                  style={{fontSize: 26, color: 'black'}}/>
               </Button>
               <Button 
               transparent 
-              style={{marginLeft: 127}}
+              style={{marginLeft: 142}}
                   >
                 <Icon 
-                  name='open-in-new' 
+                  name='play-circle' 
                   type='MaterialCommunityIcons' 
                   style={{fontSize: 20, color: 'black'}}/>
               </Button>
@@ -238,7 +226,7 @@ class BooksArticles extends Component {
   }
 }
 
-export default BooksArticles;
+export default Downloads;
 
 const styles = StyleSheet.create({
   container: {
--- src/Media/TabA.js
+++ src/Media/TabA.js
@@ -45,7 +45,12 @@ class TabA extends Component {
           <CardItem style={{width: 300}}>
             <Icon name='ios-musical-notes' type='Ionicons' />
             <Text style={{fontSize: 16}}>Music</Text>
-              <Button transparent style={{marginLeft: 136}}>
+              <Button transparent 
+              style={{marginLeft: 136}}
+              onPress={() =>
+                this.props.navigation.navigate('Music')
+              }
+              >
                 <Icon 
                   name='chevron-circle-right' 
                   type='FontAwesome' 
@@ -57,7 +62,11 @@ class TabA extends Component {
           <CardItem style={{width: 300}}>
             <Icon name='podcast' type='FontAwesome' />
             <Text style={{fontSize: 16}}>Podcasts</Text>
-              <Button transparent style={{marginLeft: 112.25}}>
+              <Button transparent 
+              style={{marginLeft: 112.25}}
+              onPress={() =>
+                this.props.navigation.navigate('Podcasts')
+              }>
                 <Icon 
                   name='chevron-circle-right' 
                   type='FontAwesome' 
@@ -69,7 +78,11 @@ class TabA extends Component {
           <CardItem style={{width: 300}}>
             <Icon name='download' type='Entypo' />
             <Text style={{fontSize: 16}}>Your Downloaded Items</Text>
-              <Button transparent style={{marginLeft: 7.5}}>
+              <Button transparent 
+              style={{marginLeft: 7.5}}
+              onPress={() =>
+                this.props.navigation.navigate('Downloads')
+              }>
                 <Icon 
                   name='chevron-circle-right' 
                   type='FontAwesome' 
